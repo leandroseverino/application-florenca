9a284337e791f5a27cf7960500ed2b2d6aca8dcd
leandroseverino <lseverino@gmail.com>
2016-06-12 16:54:56 -0300

    first commit to implements contact form.

--- bin/contact_me.php
+++ bin/contact_me.php
@@ -6,21 +6,21 @@ if(empty($_POST['name'])  		||
    empty($_POST['message'])	||
    !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
    {
-	echo "No arguments Provided!";
+	echo "Dados inválidos fornecidos !";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// create email body and send it	
-$to = 'your-email@your-domain.com'; // PUT YOUR EMAIL ADDRESS HERE
-$email_subject = "Modern Business Contact Form:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
-$email_body = "You have received a new message from your website's contact form.\n\n"."Here are the details:\n\nName: $name\n\nPhone: $phone\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@your-domain.com\n";
-$headers .= "Reply-To: $email_address";	
+
+// create email body and send it
+$to = 'contato@imobiliariaflorenca.com.br'; // PUT YOUR EMAIL ADDRESS HERE
+$email_subject = "Mensagem enviada pelo site por:  $name"; // EDIT THE EMAIL SUBJECT LINE HERE
+$email_body = "Você recebeu uma nova mensagem do form de contato do website da Imobiliaria Florença.\n\n"."Aqui estão os Detalhes:\n\nRemetente: $name\n\nTelefone: $phone\n\nE-mail: $email_address\n\nMensagem:\n$message";
+$headers = "From: contato@imobiliariaflorenca.com.br\n";
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
