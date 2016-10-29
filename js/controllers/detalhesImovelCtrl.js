f94ebe9ab49947478e63e270cd174d5f9aef45f9
leandroseverino <lseverino@gmail.com>
2016-10-29 19:55:52 -0200

    adiciona a lista de imoveis relacionados

--- js/controllers/detalhesImovelCtrl.js
+++ js/controllers/detalhesImovelCtrl.js
@@ -1,4 +1,4 @@
-angular.module("appSite").controller("detalhesImovelCtrl", ["$scope", "$routeParams", "imovel", "imoveisRelacionados", function ($scope, $routeParams, imovel, imoveisRelacionado) {
+angular.module("appSite").controller("detalhesImovelCtrl", ["$scope", "$routeParams", "imovel", "imoveisRelacionados", function ($scope, $routeParams, imovel, imoveisRelacionados) {
     $scope.imovel = imovel.data;
-    $scope.imoveisRelacionados = []; // imoveisRelacionados.data
+    $scope.imoveisRelacionados = imoveisRelacionados.data;
 }]);
--- js/services/detalhesImovelAPIService.js
+++ js/services/detalhesImovelAPIService.js
@@ -1,11 +1,9 @@
 angular.module("appSite").factory("detalhesImovelAPI", ["$http", "config", function($http, config) {
     var _getImovel = function (slug) {
-        console.log("slug : " + slug);
         return $http.get(config.baseUrl + "/api/imovel/" + slug);
     };
 
     var _getImoveisRelacionados = function (slug) {
-        console.log("slug : " + slug);
         return $http.get(config.baseUrl + "/api/imoveis-relacionados/" + slug);
     };
 
