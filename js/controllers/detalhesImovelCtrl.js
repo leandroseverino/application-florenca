cf5f431fc87e16e962fe701d992e900e6e8f47e0
leandroseverino <lseverino@gmail.com>
2017-07-31 20:41:30 -0300

    Resolve o compartilhamento da foto de destaque

--- js/controllers/detalhesImovelCtrl.js
+++ js/controllers/detalhesImovelCtrl.js
@@ -10,10 +10,23 @@ angular.module("appSite").controller("detalhesImovelCtrl", ["$scope",
     $scope.imovel = imovel.data;
     $scope.root_url;
 
+    $scope.foto_destaque_path = 'http://www.imobiliariaflorenca.com.br/website/media/imovel_recurso/';
+    $scope.foto_destaque =  $scope.foto_destaque_path + 'img_default.png';
+    $scope.obtem_foto_destaque = function() {
+        for (var i = 0; i < $scope.imovel.recursos.length; i++) {
+            if ($scope.imovel.recursos[i].tipo_recurso == 'Foto') {
+                var image_path = $scope.imovel.recursos[i].resource_path;
+                image_path = image_path.split('/')[1];
+                $scope.foto_destaque = $scope.foto_destaque_path + image_path;
+                break;
+            }            
+        }
+    }
+    foto_destaque = $scope.obtem_foto_destaque();
     $scope.myModel = {
               Url: 'http://www.imobiliariaflorenca.com.br/#/detalhe-imovel/' + $scope.imovel.slug,
               Name: "Detalhes do imóvel: " + $scope.imovel.codigo, 
-              ImageUrl: 'http://www.imobiliariaflorenca.com.br/website/media/imovel_recurso/img_default.png'
+              ImageUrl: $scope.foto_destaque
     };
 
     var _data = loaded_parameters.data;
